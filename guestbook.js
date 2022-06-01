da09aadf1578433f9686bb409fd158b5432b38e5
hoangnam-dt <58728466+hoang1510-dt@users.noreply.github.com>
2022-06-01 11:23:21 +0700

    change api

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://e1159a1a.au-syd.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
